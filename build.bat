b99d34f3831349967a7f200b3f982cc060e87517
Rifat Ibn Azad <33044977+RifsxD@users.noreply.github.com>
2023-07-07 23:07:34 +0600

    build methods

---
+++ build.bat
@@ -0,0 +1,88 @@
+@echo off
+
+REM Sets Variable & And Pull Infro From File.
+set /p version=<version.md
+
+ECHO ****************************************************
+ECHO ************** RXD - MODPACK - XTENDED *************
+ECHO ************ Source Version - %version% ************
+ECHO ****************************************************
+ECHO Starting Source Code Packaging!
+
+timeout -1
+
+ECHO ------------------------------------------
+
+REM Does Cleaning Of Old Cache.
+
+rd /s /q "pack\pc\Data\" && rd /s /q "pack\mobile\Data\"
+
+ECHO Cleaned Cache Data For Pack - PC - MOBILE!
+
+timeout 5
+
+REM Does Pack For PC Platform.
+
+ECHO Copying Data To Pack-PC!
+
+xcopy /s /y "src\*" "pack\pc\Data\"
+
+ECHO Copyied Data To Pack-PC!
+
+timeout 2
+
+cd "pack\pc\Data\"
+
+ECHO Selecting GPU Specific Textures - DX11!
+
+del /S "*.pvr"
+
+ECHO Selected GPU Specific Textures - DX11!
+
+timeout 2
+
+ECHO Encoding To LZ4 Compression - DVPL!
+
+dvpl cp
+
+ECHO Encoded To LZ4 Compression - DVPL!
+
+timeout 2
+
+REM Does Fallback To Source Path.
+
+cd "../../.."
+
+REM Does Pack For MOBILE Platform.
+
+ECHO Copying Data To Pack-MOBILE!
+
+xcopy /s /y "src\*" "pack\mobile\Data\"
+
+ECHO Copyied Data To Pack-MOBILE!
+
+timeout 2
+
+cd "pack\mobile\Data\"
+
+ECHO Selecting GPU Specific Textures - ASTC!
+
+del /S "*.dds"
+
+ECHO Selected GPU Specific Textures - ASTC!
+
+timeout 2
+
+ECHO Encoding To LZ4 Compression - DVPL!
+
+dvpl cp
+
+ECHO Encoded To LZ4 Compression - DVPL!
+
+ECHO ------------------------------------------
+
+ECHO Finished Packaging Process!
+
+ECHO ------------------------------------------
+
+pause
