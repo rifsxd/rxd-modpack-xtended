01ba5d63dee870ddaf00921f6d7ade96aa77c975
Rifat Ibn Azad <33044977+RifsxD@users.noreply.github.com>
2023-07-10 22:10:15 +0600

    source code bump -> 2.3.0

--- build.bat
+++ build.bat
@@ -1,44 +1,65 @@
 @echo off
 
-REM Sets Variable & And Pull Infro From File.
+REM Sets Variable & And Pull Info From File.
+
+REM Commenting Unused Code setlocal EnableExtensions EnableDelayedExpansion
+
 set /p version=<version.md
 set /p verbose=<verbose.md
 set versioning=version.md
 set modv=xtendedmodversion.txt
+set build=buildtime.txt
+
+TITLE XTENDED - COMPILER %version%
 
 CLS
+
 :MENU
+ECHO.
+systeminfo | findstr /B /C:"OS Name" /C:"OS Version" /C:"System Type"
 ECHO.
 ECHO *****************************************************************
 ECHO ************** RXD - MODPACK - XTENDED **************************
 ECHO ************ Source Version - %version% ***********************
-ECHO *****************************************************************
+ECHO ***************************************************************** 
 ECHO.
 ECHO Starting Source Code Compiler... %time%
 ECHO.
-ECHO ...................................................................
+ECHO .........................................................................
 ECHO ..... PRESS 1, 2, 3, 4, 5, c OR v to select your task, or e to EXIT .....
-ECHO ...................................................................
+ECHO .........................................................................
 ECHO.
 ECHO 1 - Compile For PC - DX11
 ECHO 2 - Compile For MOBILE - ASTC
 ECHO 3 - Compile For MOBILE - Lesta - ASTC
 ECHO 4 - Compile For MOBILE-EMBED - ASTC
-ECHO 5 - Compile For All ( PC - DX11 ) , ( MOBILE - ASTC ) And ( MOBILE - Lesta - ASTC )
-ECHO c - Clean WwiseCache
+ECHO 5 - Compile For All ( PC - DX11 ) , ( MOBILE - ASTC ) , ( MOBILE - Lesta - ASTC ) And ( MOBILE-EMBED - ASTC )
+ECHO w - Include Wwise Bank Files To Source
 ECHO v - Verbose Source Code Commits
 ECHO e - EXIT
 ECHO.
-SET /P M=Type 1, 2, 3, 4, 5, c, v or e then press ENTER:
+SET /P "M=Type 1, 2, 3, 4, 5, c, v or e then press ENTER: "
 ECHO.
-IF %M%==1 GOTO DX11
-IF %M%==2 GOTO ASTC
-IF %M%==3 GOTO ASTC-LESTA
-IF %M%==4 GOTO EMBED
-IF %M%==5 GOTO ALL
-IF %M%==c GOTO CLEAN
-IF %M%==v GOTO VER
-IF %M%==e GOTO EOF
+IF "%M%" EQU "" GOTO ERROR
+IF "%M%" EQU "1" GOTO DX11
+IF "%M%" EQU "2" GOTO ASTC
+IF "%M%" EQU "3" GOTO ASTC-LESTA
+IF "%M%" EQU "4" GOTO EMBED
+IF "%M%" EQU "5" GOTO ALL
+IF "%M%" EQU "w" GOTO WWISE
+IF "%M%" EQU "v" GOTO VER
+IF "%M%" EQU "e" GOTO EOF
+
+
+:ERROR
+
+ECHO.
+ECHO Invalid Input! Choose Correctly -,- 
+ECHO.
+
+@pause
+
+GOTO MENU
 
 :DX11
 
@@ -100,6 +121,8 @@ ECHO Finished Packaging Process For DX11!
 ECHO --------------------X----------------------
 ECHO.
 
+powershell date > ./%build%
+
 REM Does Fallback To Source Path.
 
 cd "../../.."
@@ -146,6 +169,7 @@ del /S "*.dds"
 
 del /S "*.pvr"
 
+
 ECHO.
 ECHO Cleared Unused GPU Specific Textures - ASTC!
 ECHO.
@@ -159,6 +183,8 @@ ECHO.
 del /S "*.yaml"
 
 del /S "keyart*.webp"
+
+rd /s /q "UI"
 
 
 ECHO.
@@ -185,6 +211,8 @@ ECHO Finished Packaging Process For ASTC!
 ECHO --------------------X----------------------
 ECHO.
 
+powershell date > ./%build%
+
 REM Does Fallback To Source Path.
 
 cd "../../.."
@@ -203,17 +231,18 @@ ECHO.
 
 GOTO MENU
 
-:CLEAN
+:WWISE
 
 ECHO.
-ECHO Cleaning WwiseCache!
+ECHO Including Wwise Bank Files To SOURCE!
+ECHO.
 
-del "src\WwiseSound\Init.bnk"
-del "src\WwiseSound\*.json"
-del "src\WwiseSound\*.xml"
-rd /s /q "src\WwiseSound\English(US)\"
+del /S "cache\Init.bnk"
 
-ECHO Cleaned WwiseCache!
+xcopy /s /y  "cache\*.bnk" "src\WwiseSound\"
+
+ECHO.
+ECHO Included Wwise Bank Files  To SOURCE!
 ECHO.
 
 GOTO MENU
@@ -278,6 +307,8 @@ ECHO Finished Packaging Process For DX11!
 ECHO --------------------+----------------------
 ECHO.
 
+powershell date > ./%build%
+
 REM Does Fallback To Source Path.
 
 cd "../../..
@@ -334,6 +365,8 @@ del /S "*.yaml"
 
 del /S "keyart*.webp"
 
+rd /s /q "UI"
+
 ECHO.
 ECHO Cleaned Embed Specific Configs!
 ECHO.
@@ -357,6 +390,8 @@ ECHO Finished Packaging Process For ASTC!
 
 ECHO --------------------+----------------------
 ECHO.
+
+powershell date > ./%build%
 
 REM Does Fallback To Source Path.
 
@@ -414,6 +449,8 @@ del /S "*.yaml"
 
 del /S "keyart*.webp"
 
+rd /s /q "UI"
+
 
 ECHO.
 ECHO Cleaned Embed Specific Configs!
@@ -432,18 +469,96 @@ ECHO Encoded To LZ4 Compression - DVPL!
 ECHO.
 
 ECHO.
-ECHO --------------------X----------------------
+ECHO --------------------+----------------------
 
 ECHO Finished Packaging Process For ASTC - Lesta!
 
-ECHO --------------------X----------------------
+ECHO --------------------+----------------------
 ECHO.
+
+powershell date > ./%build%
 
 REM Does Fallback To Source Path.
 
 cd "../../.."
 
 type .\%versioning% > ./pack/mobile-lesta/data/%modv%
+
+ECHO ------------------------------------------
+
+REM Does Cleaning Of Old Cache For MOBILE-EMBED.
+
+rd /s /q "pack\mobile-embed\Data\"
+
+ECHO.
+ECHO Cleaned Cache Data For Pack - MOBILE-EMBED!
+ECHO.
+
+timeout 5
+
+REM Does Pack For MOBILE-EMBED Platform.
+
+ECHO.
+ECHO Copying Data To Pack - MOBILE-EMBED!
+ECHO.
+
+xcopy /s /y "src\*" "pack\mobile-embed\Data\"
+
+ECHO.
+ECHO Copyied Data To Pack - MOBILE-EMBED!
+ECHO.
+
+timeout 2
+
+cd "pack\mobile-embed\Data\"
+
+ECHO.
+ECHO Selecting GPU Specific Textures - ASTC!
+ECHO.
+
+del /S "*.dds"
+
+ECHO.
+ECHO Selected GPU Specific Textures - ASTC!
+ECHO.
+
+timeout 2
+
+ECHO.
+ECHO Encoding To LZ4 Compression - DVPL!
+ECHO.
+
+..\..\..\bin\dvplc.exe encode ./ --replace --force
+
+ECHO.
+ECHO Encoded To LZ4 Compression - DVPL!
+ECHO.
+
+ECHO.
+ECHO Decoding To LZ4 Compression - DVPL!
+ECHO.
+
+..\..\..\bin\dvplc.exe decode ./ --replace --force
+
+ECHO.
+ECHO Decoded To LZ4 Compression - DVPL!
+ECHO.
+
+ECHO.
+ECHO --------------------+----------------------
+
+ECHO Finished Packaging Process For ASTC-EMBED!
+
+ECHO --------------------+----------------------
+ECHO.
+
+powershell date > ./%build%
+
+REM Does Fallback To Source Path.
+
+cd "../../.."
+
+type .\%versioning% > ./pack/mobile-embed/data/%modv%
 
 ECHO.
 ECHO --------------------X----------------------
@@ -525,6 +640,8 @@ ECHO Finished Packaging Process For ASTC-EMBED!
 ECHO --------------------+----------------------
 ECHO.
 
+powershell date > ./%build%
+
 REM Does Fallback To Source Path.
 
 cd "../../.."
@@ -585,6 +702,8 @@ del /S "*.yaml"
 
 del /S "keyart*.webp"
 
+rd /s /q "UI"
+
 
 ECHO.
 ECHO Cleaned Embed Specific Configs!
@@ -609,6 +728,8 @@ ECHO Finished Packaging Process For ASTC - Lesta!
 
 ECHO --------------------X----------------------
 ECHO.
+
+powershell date > ./%build%
 
 REM Does Fallback To Source Path.
 
--- clean.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-del "src\WwiseSound\Init.bnk"
-del "src\WwiseSound\*.json"
-del "src\WwiseSound\*.xml"
-rd /s /q "src\WwiseSound\English(US)\"
-@pause
